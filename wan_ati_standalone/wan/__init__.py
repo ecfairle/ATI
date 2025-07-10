08f684907f9b48db0cdff5c5f4fc088768749753
e <e>
2025-07-10 11:19:37 -0700

    remove extra imports

--- wan_ati_standalone/wan/__init__.py
+++ wan_ati_standalone/wan/__init__.py
@@ -2,5 +2,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from . import configs, distributed, modules
-from .first_last_frame2video import WanFLF2V
 from .image2video import WanATI
--- wan_ati_standalone/wan/configs/__init__.py
+++ wan_ati_standalone/wan/configs/__init__.py
@@ -5,18 +5,8 @@ import os
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
 from .wan_i2v_14B import i2v_14B
-from .wan_t2v_1_3B import t2v_1_3B
-from .wan_t2v_14B import t2v_14B
 
-# the config of t2i_14B is the same as t2v_14B
-t2i_14B = copy.deepcopy(t2v_14B)
-t2i_14B.__name__ = 'Config: Wan T2I 14B'
-
-# the config of flf2v_14B is the same as i2v_14B
-flf2v_14B = copy.deepcopy(i2v_14B)
-flf2v_14B.__name__ = 'Config: Wan FLF2V 14B'
-flf2v_14B.sample_neg_prompt = "镜头切换，" + flf2v_14B.sample_neg_prompt
-
+# Only include the ATI-14B config for this standalone version
 WAN_CONFIGS = {
     'ati-14B': i2v_14B,
 }
